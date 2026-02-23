6a2883ce7111eb24b344ecff0691105ce5ee3e5c
Y.Takeuchi <kaidouji85@gmail.com>
2026-02-23 14:35:09 +0900

    受信ページにRemote DescriptionとRemote ICE Candidateの入力フィールドを追加し、スタイルを調整

--- src/receiver.ts
+++ src/receiver.ts
@@ -6,5 +6,13 @@ const app =
 app.innerHTML = `
   <div>
     <h1>受信ページ (Receiver)</h1>
+    <h2>Remote Description</h2>
+    <textarea class="description-input" id="remote-description" placeholder="Remote Descriptionを入力してください"></textarea>
+    <h2>Remote ICE Candidate</h2>
+    <textarea class="ice-input" id="remote-ice-candidate" placeholder="Remote ICE Candidateを入力してください"></textarea>
   </div>
 `;
+
+window.onload = async () => {
+  const connection = new RTCPeerConnection();
+};
