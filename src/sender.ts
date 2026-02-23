cee2ecbbc87c6939228d18c8108d731c9aa5740a
Y.Takeuchi <kaidouji85@gmail.com>
2026-02-23 12:42:03 +0900

    送信ページのエントリポイントを追加し、ICE候補の待機処理を実装

--- src/sender.ts
+++ src/sender.ts
@@ -8,3 +8,24 @@ app.innerHTML = `
     <h1>送信ページ (Sender)</h1>
   </div>
 `;
+
+const waitUntilIceCandidate = (connection: RTCPeerConnection) =>
+  new Promise((resolve) => {
+    connection.addEventListener("icecandidate", resolve);
+  });
+
+/**
+ * エントリポイント
+ */
+window.onload = async () => {
+  console.log("Sender page loaded");
+  const connection = new RTCPeerConnection();
+  const sendChannel = connection.createDataChannel("sendDataChannel");
+  const description = await connection.createOffer();
+  console.log("Offer created:", description);
+  const [iceCandidateEvent] = await Promise.all([
+    waitUntilIceCandidate(connection),
+    connection.setLocalDescription(description)
+  ]);
+  console.log("ICE candidate event:", iceCandidateEvent);
+};
