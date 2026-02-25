81e47efdda87849a1014b1e8fe9149110245702f
Y.Takeuchi <kaidouji85@gmail.com>
2026-02-26 00:29:44 +0900

    コネクションステートの表示機能を追加し、接続状態の更新処理を実装

--- src/receiver.ts
+++ src/receiver.ts
@@ -13,28 +13,49 @@ import {
 } from "./dom/remote-info";
 import { CONNECT_HTML, getConnectButtonElement } from "./dom/connect";
 import { getAppElement } from "./dom/app";
+import {
+  CONNECTION_STATE_HTML,
+  refreshConnectionState,
+} from "./dom/connection-state";
 
 /** アプリのルートHTML要素 */
 const app = getAppElement();
 app.innerHTML = `
   <div>
     <h1>受信ページ (Receiver)</h1>
+    ${CONNECTION_STATE_HTML}
     ${REMOTE_INFO_HTML}
     ${CONNECT_HTML}
     ${OWN_INFO_HTML}
   </div>
 `;
 
+/** WebRTCコネクション、nullは未接続 */
+let connection: RTCPeerConnection | null = null;
+
+/**
+ * コネクションステートが変化したときの処理
+ */
+const onConnectionStateChange = () => {
+  if (!connection) {
+    throw new Error("接続が初期化されていません");
+  }
+
+  refreshConnectionState(connection.connectionState);
+};
+
 /**
  * 接続ボタンが押されたときの処理
  */
 const onConnectButtonPushed = async () => {
-  const connection = new RTCPeerConnection();
+  connection = new RTCPeerConnection();
+  refreshConnectionState(connection.connectionState);
+  connection.addEventListener("connectionstatechange", onConnectionStateChange);
   const remoteDescription = getRemoteRTCSessionDescription();
   const remoteIceCandidates = getRemoteRTCIceCandidates();
   await connection.setRemoteDescription(remoteDescription);
   await Promise.all(
-    remoteIceCandidates.map((c) => connection.addIceCandidate(c)),
+    remoteIceCandidates.map((c) => connection?.addIceCandidate(c)),
   );
   const description = await connection.createAnswer();
   const [iceCandidates] = await Promise.all([
