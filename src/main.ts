01363f1b9eb7dcb3090103e006880ec690e7cbb5
Y.Takeuchi <kaidouji85@gmail.com>
2026-02-27 22:57:01 +0900

    style: improve HTML structure and update CSS classes for main content

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ if (!app) {
 }
 
 app.innerHTML = `
-	<div>
-		<h1>Local Only WebRTC Sample</h1>
-		<p class="app-introduction">送信ページと受信ページを別エントリに分けたサンプルです。</p>
-		<p><a href="/sender.html">送信ページを開く</a></p>
-		<p><a href="/receiver.html">受信ページを開く</a></p>
+	<div class="main">
+		<h1 class="main__title">Local Only WebRTC Sample</h1>
+		<p class="main__introduction">送信ページと受信ページを別エントリに分けたサンプルです。</p>
+		<a class="main__sender" href="/sender.html">送信ページを開く</a>
+		<a class="main__receiver" href="/receiver.html">受信ページを開く</a>
 	</div>
 `;
